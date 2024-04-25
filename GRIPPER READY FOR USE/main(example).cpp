772f01936247d0464dab529d92718b8731b6c800
magud23 <magud23@student.sdu.dk>
2024-04-25 09:10:56 +0200

    added explanation in main(example)

---
+++ GRIPPER READY FOR USE/main(example).cpp
@@ -0,0 +1,28 @@
+
+#include "gripper.h"
+// use gripperDummy.h if you want to test without gripper.'
+
+// install SQLITE and QT-sqlite driver: 
+// 		sudo apt install sqlite3
+// 		sudo apt install libqt5sql5-sqlite
+
+// HAVE THIS IN CMAKE.TXT for non :
+// 		find_package(Qt5Sql REQUIRED)
+// 		target_link_libraries("projectName" Qt5::Core Qt5::Sql)
+
+
+
+int main()
+{
+
+    Gripper bigbertha;
+    
+
+    bigbertha.gClose();
+
+
+    bigbertha.gOpen();
+
+
+    return 0;
+}
