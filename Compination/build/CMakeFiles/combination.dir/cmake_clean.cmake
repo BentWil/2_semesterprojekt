099e69d6db06c23812fd4556534fcf926e286bae
Kasper7979 <162568876+Kasper7979@users.noreply.github.com>
2024-04-25 08:55:02 +0200

    Creation of Combination

---
+++ Compination/build/CMakeFiles/combination.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/combination.dir/main.cpp.o"
+  "CMakeFiles/combination.dir/main.cpp.o.d"
+  "combination"
+  "combination.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/combination.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
